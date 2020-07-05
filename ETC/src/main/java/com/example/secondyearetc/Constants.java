e389cd2d33a53f6bc7f5fcb15fca63a0cc428b1d
hp <krishnaashar20@gmail.com>
2020-07-05 21:56:06 +0530

    Initial commit

---
+++ ETC/src/main/java/com/example/secondyearetc/Constants.java
@@ -0,0 +1,6 @@
+package com.example.secondyearetc;
+
+public class Constants {
+    public static final String STORAGE_PATH_UPLOADS = "uploads_electrical/";
+    public static final String DATABASE_PATH_UPLOADS = "uploads_electrical";
+}
---
+++ Mech/src/main/java/com/example/secondyearmech/Constants.java
@@ -0,0 +1,6 @@
+package com.example.secondyearmech;
+
+public class Constants {
+    public static final String STORAGE_PATH_UPLOADS = "uploads_mech/";
+    public static final String DATABASE_PATH_UPLOADS = "uploads_mech";
+}
